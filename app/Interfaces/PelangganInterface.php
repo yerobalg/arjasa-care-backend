0dcfe0602795470dfa63faebf0dd363aebc5acb6
yerobalg <74844505+yerobalg@users.noreply.github.com>
2022-04-25 12:46:32 +0700

    created get pelanggan repository with pagination and searching

--- app/Interfaces/PelangganInterface.php
+++ app/Interfaces/PelangganInterface.php
@@ -4,9 +4,9 @@ namespace App\Interfaces;
 use App\Models\Pelanggan;
 
 interface PelangganInterface {
-  public function getByNoHP($username);
+  public function getByNomorHP($nomorHp);
   public function create($data);
   public function update(Pelanggan $pelanggan, $data);
   public function delete(Pelanggan $pelanggan);
-  public function getPelanggan($page);
+  public function getPelanggan($page, $keyword);
 }
--- app/Repositories/PelangganRepository.php
+++ app/Repositories/PelangganRepository.php
@@ -1,4 +1,45 @@
 <?php
 
-use App\Interfaces\PengurusApotekInterface;
+use App\Interfaces\PelangganInterface;
 use App\Models\Pelanggan;
+
+class PelangganRepository implements PelangganInterface
+{
+  public function getPelanggan($page, $keyword)
+  {
+    $offset = ($page - 1) * 10;
+
+    if ($keyword != '') {
+      $keyword = strtolower($keyword);
+      $pelanggan = Pelanggan::where(
+        'lower(nama)',
+        'like',
+        '%' . $keyword . '%'
+      )
+        ->orWhere('lower(nomor_hp)', 'like', '%' . $keyword . '%')
+        ->offset($offset)->limit(10)->get();
+      $total = Pelanggan::where(
+        'lower(nama)',
+        'like',
+        '%' . $keyword . '%'
+      )
+        ->orWhere('lower(nomor_hp)', 'like', '%' . $keyword . '%')
+        ->count();
+    } else {
+      $pelanggan = Pelanggan::offset($offset)->limit(10)->get();
+      $total = Pelanggan::count();
+    }
+
+    return [
+      'data' => $pelanggan,
+      'currentPage' => $page,
+      'totalPage' => ceil($total / 10),
+      'totalData' => $total
+    ];
+  }
+
+  public function getByNomorHP($nomorHp)
+  {
+    
+  }
+}
